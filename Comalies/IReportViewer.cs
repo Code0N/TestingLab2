1634844c5edae798976c4f8734a1da6927f29388
Code0N <redeemer.of.souls@yandex.ru>
2018-04-02 16:13:34 +0300

    Добавлены интерфейсы

---
+++ Comalies/IReportViewer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comalies
+{
+    interface IReportViewer
+    {
+        int UsedSize { get; set; }
+        //============================
+        void Clean();
+    }
+}
