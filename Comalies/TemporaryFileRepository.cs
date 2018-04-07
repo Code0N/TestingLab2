651f3e152a2d983f02afa7cf3c0dfaba890455fd
Code0N <redeemer.of.souls@yandex.ru>
2018-04-07 18:05:29 +0300

    Реализован TemporaryFileRepository

---
+++ Comalies/TemporaryFileRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comalies
+{
+    class TemporaryFileRepository
+    {
+        private List<string> FileNamesList;
+        private IStringFormatter StringFormatter { get; set; }
+
+        public TemporaryFileRepository()
+        {
+            FileNamesList = new List<string>();
+        }
+
+        public string GetLastElement()
+        {
+            return FileNamesList.Last();
+        }
+
+        public void Add(string FileName)
+        {
+            try
+            {
+                FileNamesList.Add(StringFormatter.ShortFileString(FileName));
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Ты зачем null передал, нехороший человек?" + Environment.NewLine + ex.Message, "Ошыбка", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
