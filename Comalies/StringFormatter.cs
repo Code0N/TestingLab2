b3ea7ec32a4f476eb6ef75d0cb94ababad01f6c1
Code0N <redeemer.of.souls@yandex.ru>
2018-04-07 17:36:24 +0300

    Реализован StringFormatter для задания 2-3 (из прошлой лабы, Ctrl+C, Ctrl+V method)

---
+++ Comalies/StringFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Comalies
+{
+    public class StringFormatter: IStringFormatter
+    {
+        public string ShortFileString(string path)
+        {
+            if (path == String.Empty)
+                return path;
+            if (path == null)
+                throw new NullReferenceException("String is Null");
+            return (Path.ChangeExtension(Path.GetFileName(path), ".TMP")).ToUpperInvariant();
+        }
+    }
+}
